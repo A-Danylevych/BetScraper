7bcaf2c7dbafe5e067b76212ce2fab35f5cfc039
Cerpany <54603203+A-Danylevych@users.noreply.github.com>
2023-10-23 15:40:53 +0300

    Update app

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 import json
 from flask import Flask, request, jsonify
 from scrapy.crawler import CrawlerProcess
-from betscraper.spiders.draftkingsspider import DraftkingsspiderSpider
+from scrapy.utils.project import get_project_settings
+from multiprocessing import Process
 
 app = Flask(__name__)
 
-@app.route('/run-spider', methods=['GET'])
-def run_spider():
-    process = CrawlerProcess(settings={
-        'FEED_FORMAT': 'json',
-        'FEED_URI': 'output.json', 
-    })
-    process.crawl(DraftkingsspiderSpider)
-    process.start() 
 
+def run_spider():
+    process = CrawlerProcess(get_project_settings())
+    process.crawl('draftkingsspider')
+    process.start()
+
+
+@app.route('/run-spider', methods=['GET'])
+def start_spider():
+    spider_process = Process(target=run_spider)
+    spider_process.start()
+    spider_process.join()
     return jsonify({'message': 'Spider is running'})
+
 
 @app.route('/get-scraped-data', methods=['GET'])
 def get_scraped_data():
     try:
-        with open('output.json', 'r') as file:
+        with open('betdata.json', 'r') as file:
             data = json.load(file)
         return jsonify(data)
     except FileNotFoundError:
         return jsonify({'error': 'Scraped data not found'})
-    
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
